e8a1b4f78878cf35138ec8c4a23eae63b1fd58fd
Naim Altınel <naimaltinel@gmail.com>
2022-02-04 01:56:02 +0300

    Metot
    Metot Tanımlama Ders Notları

---
+++ metot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erişim belirteci , geri dönüş tipi , metot adı (parametre listesi/argümanlar)
+            //{
+                    //Komutlar
+                    //return
+            //}
+
+            int a=2, b=3;
+
+            Console.WriteLine(a+b);
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+            metotlar ornek = new metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArtirVeTopla(ref a,ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1+deger2);
+        }
+    }
+    class metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArtirVeTopla(ref int deger1,ref int deger2)
+        {
+            deger1++;
+            deger2++;
+            return deger1+deger2;
+        }
+    }
+}
