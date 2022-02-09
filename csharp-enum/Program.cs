8bae7211f77094b9d3289e18daa7b987f2c783bf
Naim Altınel <naimaltinel@gmail.com>
2022-02-10 01:07:43 +0300

    Csharp-Enum
    Enum Ders Notları

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+            if(sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekle");
+            }
+            else if(sicaklik>=(int)HavaDurumu.sıcak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            else if(sicaklik>=(int)HavaDurumu.Normal && sicaklik<=(int)HavaDurumu.CokSıcak)
+            {
+                Console.WriteLine("Hadi dışarıya çıkalım!");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal=20,
+        sıcak=25,
+        CokSıcak=30
+    }
+}
